7021f5cf294727ca31b4c3f86a83e0a822855be7
esekh <sekhwerewolf@gmail.com>
2022-01-15 16:40:56 +0300

    Homeworks (UPD6)
    Выгрузка домашних работ №1, №2, №3, №4 (без 3, 4, 5-го заданий), №5 (без 3 и 4-го заданий) и №6 (без 3 и 4-го заданий) + .gitignore.

---
+++ hw6TASK1/Program.cs
@@ -0,0 +1,48 @@
+﻿// ВЫПОЛНИЛА: СЕХИНА ЕЛЕНА
+
+// Задание №1. Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double (double, double). 
+// Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+
+using System;
+
+public delegate double Func(double a, double x);
+class Program
+{
+    // Метод, принимающий делегат:
+    public static void Table(Func F, double a, double x, double b)
+    {
+        Console.WriteLine("----- A ------- X -------- Y -----");
+        while (x <= b)
+        {
+            Console.WriteLine("| {0,8:0.000} | {1,8:0.000} | {2,8:0.000} |", a, x, F(a, x));
+            x += 1;
+        }
+        Console.WriteLine("-----------------------------------");
+    }
+
+    // Метод возвращает значение функции a*x^2:
+    public static double MyFunc(double a, double x)
+    {
+        return a * x * x;
+    }
+
+    // Метод возвращает значение функции a*sin(x):
+    public static double MySin(double a, double x)
+    {
+        return a * Math.Sin(x);
+    }
+
+    static void Main()
+    {
+        // Создаем новый делегат и передаем ссылку на него в метод Table:
+        Console.WriteLine("Таблица функции a*x^2:");
+
+        // Параметры метода и тип возвращаемого значения, должны совпадать с делегатом:
+        Table(new Func(MyFunc), -1.5, -2, 2);
+
+        Console.WriteLine("Таблица функции a*sin(x):");
+        Table(new Func(MyFunc), 3, -2, 2);
+
+        Console.ReadKey();
+    }
+}
